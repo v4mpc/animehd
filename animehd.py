385941c35b183022116a71eab0f23d45bd3c0d0e
Yona Mahundi <yona_mahundi@tz.jsi.com>
2020-09-08 00:24:33 +0300

    resume download is working

--- animehd.py
+++ animehd.py
@@ -35,47 +35,19 @@ def video_exists(destination_path, file_name, link):
     # file_name and file_size match
     file_list = os.listdir(destination_path)
     if file_name in file_list:
-        # check size
-        response = requests.head(link, headers=headers)
-        # remote_size = response.headers.get('content-length')
-        # local_size =
-        # return int(local_size) >= int(remote_size)
         return os.path.getsize(destination_path+file_name)
     return 0
-
-
-def download(link, file_name):
-    with open(destination_path+file_name, "wb") as f:
-        response = requests.get(link, stream=True, headers=headers)
-        total_length = response.headers.get('content-length')
-        if total_length:  # no content length header
-            total_length = int(total_length)
-            with click.progressbar(length=total_length,
-                                   label=file_name) as bar:
-                for data in response.iter_content(chunk_size=chunk_size):
-                    dl = len(data)
-                    f.write(data)
-                    bar.update(dl)
 
 
 def partial_download():
     file_name = f'One_Piece_03.mp4'
     link = 'https://op.manga47.net/One_Piece_Dub/03.MP4'
-    # response = requests.head(link, headers=headers)
-    # remote_size = response.headers.get('content-length')
-    # local_size = os.path.getsize(destination_path+file_name)
-    # print()
-    # print(remote_size)
-    # exit()
+
     # resume_headers = {'Range':'bytes=0-2000000'}
     with open(destination_path+file_name, "ab") as f:
         start_at = video_exists(destination_path, file_name, link)
         headers['Range'] = f'bytes={start_at}-'
-        # with open(destination_path+file_name, "ab") as f:
-
         response = requests.get(link, stream=True, headers=headers)
-        # print()
-        # exit()
         partial_length = response.headers.get('content-length')
         if partial_length:  # no content length header
             total_length = int(response.headers['Content-Range'].split('/')[1])
@@ -83,7 +55,6 @@ def partial_download():
                                    label=file_name) as bar:
                 for data in response.iter_content(chunk_size=chunk_size):
                     dl = len(data)+start_at
-                    # print(dl)
                     f.write(data)
                     bar.update(dl)
                     start_at = 0
