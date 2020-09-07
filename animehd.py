0dfc45f6bf7853be0d12217a0f119ccb6e289e33
Yona Mahundi <yona_mahundi@tz.jsi.com>
2020-09-08 01:16:31 +0300

    ready for use

--- animehd.py
+++ animehd.py
@@ -2,23 +2,14 @@
 
 import requests
 import click
-import requests
 import os
-import time
 
 
 # configuration params
 
 chunk_size = 1024
 
-resolution = {'360': '360p-0', '480': '480p-1', '720': '720p-2'}
 # destination_folder=
-
-download_type = {
-    'list': 1,
-    'single': 0
-}
-
 
 destination_path = "/home/v4mpc/Videos/one_piece/"
 
@@ -39,27 +30,33 @@ def video_exists(destination_path, file_name, link):
     return 0
 
 
-def partial_download():
-    file_name = f'One_Piece_03.mp4'
-    link = 'https://op.manga47.net/One_Piece_Dub/03.MP4'
+def partial_download(file_name, link):
 
     # resume_headers = {'Range':'bytes=0-2000000'}
-    with open(destination_path+file_name, "ab") as f:
-        start_at = video_exists(destination_path, file_name, link)
-        headers['Range'] = f'bytes={start_at}-'
-        response = requests.get(link, stream=True, headers=headers)
-        partial_length = response.headers.get('content-length')
-        if partial_length:  # no content length header
-            total_length = int(response.headers['Content-Range'].split('/')[1])
-            with click.progressbar(length=total_length,
-                                   label=file_name) as bar:
+    start_at = video_exists(destination_path, file_name, link)
+    headers['Range'] = f'bytes={start_at}-'
+    response = requests.get(link, stream=True, headers=headers)
+    partial_length = response.headers.get('content-length')
+    if partial_length:  # no content length header
+        total_length = int(response.headers['Content-Range'].split('/')[1])
+        with click.progressbar(length=total_length,
+                               label=file_name) as bar:
+            with open(destination_path+file_name, "ab") as f:
                 for data in response.iter_content(chunk_size=chunk_size):
                     dl = len(data)+start_at
                     f.write(data)
                     bar.update(dl)
                     start_at = 0
+            return
+
+    click.echo(f"{file_name}...Done")
 
 
 if __name__ == "__main__":
 
-    partial_download()
+    start_at = 57
+    while start_at < 100:
+        file_name = f'One_Piece_0{start_at}.mp4'
+        link = f'https://op.manga47.net/One_Piece_Dub/0{start_at}.MP4'
+        partial_download(file_name, link)
+        start_at += 1
